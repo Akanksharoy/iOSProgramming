aafb1ce3e3e61d2cce5b7194a44735e5adb7183f
Akanksha <akabksha.roy95@>
2023-11-01 16:34:22 +0530

    Transform

--- Chapter1/Views/Views/ViewController.swift
+++ Chapter1/Views/Views/ViewController.swift
@@ -10,31 +10,58 @@ import UIKit
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
-        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-        let v2 = UIView(frame: CGRect(x: 41, y: 56, width: 132, height: 194))
-        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
-        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
-        self.view.addSubview(v1)
-        v1.addSubview(v2)
-        self.view.addSubview(v3)
+        //        super.viewDidLoad()
+        //        let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        //        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        //        let v2 = UIView(frame: CGRect(x: 41, y: 56, width: 132, height: 194))
+        //        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        //        let v3 = UIView(frame: CGRect(x: 43, y: 197, width: 160, height: 230))
+        //        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        //        self.view.addSubview(v1)
+        //        v1.addSubview(v2)
+        //        self.view.addSubview(v3)
         
         
         
         //Bounds -- cgrect value in it's own coordinate system
+        //
+        //        let v4 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        //        v4.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        //        let v5 = UIView(frame: v4.bounds.insetBy(dx: 10, dy: 10))
+        //        v5.backgroundColor = UIColor.systemYellow
+        //        self.view.addSubview(v4)
+        //        v4.addSubview(v5)
+        //        v4.bounds.origin.x += 20
+        //        v4.bounds.origin.y += 20
         
-//        let v4 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
-//        v4.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-//        let v5 = UIView(frame: v4.bounds.insetBy(dx: 10, dy: 10))
-//        v5.backgroundColor = UIColor.systemYellow
-//        self.view.addSubview(v4)
-//        v4.addSubview(v5)
-//        v5.bounds.width+=20
-//        v5.bounds.height+=20
+        //Transform
         
+        //        let v6 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        //        v6.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        //        let v7 = UIView(frame: v6.bounds.insetBy(dx: 10, dy: 10))
+        //        v7.backgroundColor = UIColor.systemYellow
+        //        self.view.addSubview(v6)
+        //        v6.addSubview(v7)
+        //        v6.transform = CGAffineTransform(scaleX: 1.8, y: 1)
         
+//        let v1 = UIView(frame:CGRect(x: 113, y: 111, width: 132, height: 194))
+//        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+//        let v2 = UIView(frame:v1.bounds)
+//        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+//        self.view.addSubview(v1)
+//        v1.addSubview(v2)
+        
+        //        v2.transform = CGAffineTransform(translationX: 20, y: 0).rotated(by: 45 * .pi/180)
+//                v2.transform =
+//                       CGAffineTransform(rotationAngle: 45 * .pi/180).translatedBy(x: 100, y: 0)
+//        let r = CGAffineTransform(rotationAngle: 45 * .pi/180)
+//        let t = CGAffineTransform(translationX:100, y:0)
+//        v2.transform = t.concatenating(r) // not r.concatenating(t)
+//        let r = CGAffineTransform(rotationAngle: 45 * .pi/180)
+//           let t = CGAffineTransform(translationX:100, y:0)
+//           v2.transform = t.concatenating(r)
+//           v2.transform = r.inverted().concatenating(v2.transform)
+           
     }
 }
 
